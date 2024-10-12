840293d4d67b1d9ada79616e689d467287320b05
rattus-digitalis <victor.michi@coda-student.school>
2024-10-12 22:38:12 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define LARGEUR 14
+#define HAUTEUR 10
+
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+void initialiserGrille(char grille[HAUTEUR][LARGEUR]);
+void afficherGrille(char grille[HAUTEUR][LARGEUR]);
+int deplacerJoueur(char grille[HAUTEUR][LARGEUR], Position *joueur, int dx, int dy);
+int deplacerCaisse(char grille[HAUTEUR][LARGEUR], Position *caisse, int dx, int dy);
+int estGagnant(char grille[HAUTEUR][LARGEUR], Position caisse);
+int estPerdu(char grille[HAUTEUR][LARGEUR], Position caisse);
+
+#endif
